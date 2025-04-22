582b3eb7fd7780377bb3567bdb7ce6c595f42388
Jeffy Evangelista <ejeffydev@gmail.com>
2025-04-22 21:38:37 +0800

    feat: Installed zod and setup error-handler, validate-schema middleware, and drizzle config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,13 @@
 import express, { json } from "express";
 import apiRoutes from "@/routes";
+import errorHandler from "@/middlewares/error-handler.middleware";
 
 const app = express();
 
 app.use(json());
 
 app.use("/api", apiRoutes);
+
+app.use(errorHandler);
 
 export default app;
